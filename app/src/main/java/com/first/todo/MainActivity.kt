a2ef98d7012593cf05880d210e01a8a345b2f0d8
chorongs <charmblues@gmail.com>
2023-12-11 16:01:27 +0900

    ViewModel로 리팩토링

--- app/src/main/java/com/first/todo/MainActivity.kt
+++ app/src/main/java/com/first/todo/MainActivity.kt
@@ -51,32 +51,46 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+// 단계 2: `ViewModel`을 상속받은 `ToDoViewModel`을 만듭니다.
+// 첫 단계에서는 내용을 비워두고 시작합시다.
+
+// 단계 3: `TopLevel`의 파라미터로 `ToDoViewModel` 타입의
+// `viewModel`을 전달합니다. 기본 값은 `viewModel()`로 설정합시다.
+// 에러가 발생하면 아래의 `import` 문을 추가합니다.
+// `import androidx.lifecycle.viewmodel.compose.viewModel`
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopLevel() {
+    // 단계 4: text, setText를 뷰 모델로 옮겨봅시다.
+    // 뷰 모델의 프로퍼티로 변경할 경우에는 destrunction (비구조화,
+    // 구조 분해)는 사용할 수 없으니 `by`를 써봅시다.
+    // `remember`는 제거해야 합니다.
     val (text, setText) = remember { mutableStateOf("") }
+
+    // 단계 5: `toDoList`, `onSubmit`, `onEdit`, `onToggle`,
+    // `onDelete`를 모두 뷰 모델로 옮겨봅시다.
     val toDoList = remember { mutableStateListOf<ToDoData>() }
 
-    val onSubmit : (String) -> Unit = { text ->
+    val onSubmit: (String) -> Unit = {
         val key = (toDoList.lastOrNull()?.key ?: 0) + 1
-        toDoList.add(ToDoData(key, text))
+        toDoList.add(ToDoData(key, it))
         setText("")
     }
 
-    val onToggle: (Int, Boolean) -> Unit = { key, checked ->
-       val i = toDoList.indexOfFirst { it.key == key }
-        toDoList[i] = toDoList[i].copy( done = checked)
+    val onEdit: (Int, String) -> Unit = { key, newText ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(text = newText)
     }
 
-    val onDelete : (Int) -> Unit = {key ->
+    val onToggle: (Int, Boolean) -> Unit = { key, checked ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(done = checked)
+    }
+
+    val onDelete: (Int) -> Unit = { key ->
         val i = toDoList.indexOfFirst { it.key == key }
         toDoList.removeAt(i)
-    }
-
-    val onEdit: (Int, String) -> Unit = { key, text ->
-        val i = toDoList.indexOfFirst { it.key == key }
-        toDoList[i] = toDoList[i].copy( text = text)
     }
 
     Scaffold {
@@ -87,7 +101,10 @@ fun TopLevel() {
                 onSubmit = onSubmit
             )
             LazyColumn {
-                items(toDoList, key = { it.key}) {toDoData ->
+                items(
+                    items = toDoList,
+                    key = { it.key }
+                ) { toDoData ->
                     ToDo(
                         toDoData = toDoData,
                         onEdit = onEdit,
@@ -149,62 +166,59 @@ fun ToDo(
     var isEditing by remember { mutableStateOf(false) }
     Card(
         modifier = Modifier.padding(4.dp),
-
     ) {
-        Crossfade(targetState = isEditing, label = "") {
+        Crossfade(
+            targetState = isEditing,
+        ) {
             when (it) {
                 false -> {
-                    Row (
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.padding(8.dp)
-                    ){
+                    Row(
+                        modifier = Modifier.padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
                         Text(
                             text = toDoData.text,
                             modifier = Modifier.weight(1f)
                         )
-                        Text(
-                            text = "완료"
-                        )
+                        Text("완료")
                         Checkbox(
                             checked = toDoData.done,
                             onCheckedChange = { checked ->
                                 onToggle(toDoData.key, checked)
                             }
                         )
-                        Button(onClick = {
-                            isEditing = true
-                        }) {
+                        Button(
+                            onClick = { isEditing = true }
+                        ) {
                             Text("수정")
                         }
-                        Spacer(Modifier.size(4.dp))
-                        Button(onClick = {
-                            onDelete(toDoData.key)
-                        }) {
+                        Spacer(modifier = Modifier.size(4.dp))
+                        Button(
+                            onClick = { onDelete(toDoData.key) }
+                        ) {
                             Text("삭제")
                         }
                     }
                 }
-
                 true -> {
-                    Row (
-                        verticalAlignment = Alignment.CenterVertically,
-                                modifier = Modifier.padding(8.dp)
+                    Row(
+                        modifier = Modifier.padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        var (newText, setNewText) = remember { mutableStateOf(toDoData.text)}
-
+                        val (text, setText) = remember { mutableStateOf(toDoData.text) }
                         OutlinedTextField(
-                            value = newText,
-                            onValueChange = setNewText,
+                            value = text,
+                            onValueChange = setText,
                             modifier = Modifier.weight(1f)
                         )
-                        Spacer(modifier = Modifier.size(4.dp))
+                        Spacer(modifier = Modifier.size(8.dp))
                         Button(onClick = {
-                            onEdit(toDoData.key, newText)
                             isEditing = false
+                            onEdit(toDoData.key, text)
                         }) {
                             Text("완료")
                         }
-                    }   
+                    }
                 }
             }
         }
@@ -219,7 +233,6 @@ fun ToDoPreview() {
     }
 }
 
-// immutable,
 data class ToDoData(
     val key: Int,
     val text: String,
