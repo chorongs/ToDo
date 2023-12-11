9fe588af128c89f9936b03e7df9719c38e89df81
chorongs <charmblues@gmail.com>
2023-12-11 16:19:31 +0900

    ViewModel로 리팩토링

--- app/src/main/java/com/first/todo/MainActivity.kt
+++ app/src/main/java/com/first/todo/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.first.todo
 
+
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.Crossfade
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
@@ -22,16 +19,13 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.first.todo.ui.theme.ToDoTheme
 
 
@@ -53,29 +47,14 @@ class MainActivity : ComponentActivity() {
 
 // 단계 2: `ViewModel`을 상속받은 `ToDoViewModel`을 만듭니다.
 // 첫 단계에서는 내용을 비워두고 시작합시다.
-
-// 단계 3: `TopLevel`의 파라미터로 `ToDoViewModel` 타입의
-// `viewModel`을 전달합니다. 기본 값은 `viewModel()`로 설정합시다.
-// 에러가 발생하면 아래의 `import` 문을 추가합니다.
-// `import androidx.lifecycle.viewmodel.compose.viewModel`
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TopLevel() {
-    // 단계 4: text, setText를 뷰 모델로 옮겨봅시다.
-    // 뷰 모델의 프로퍼티로 변경할 경우에는 destrunction (비구조화,
-    // 구조 분해)는 사용할 수 없으니 `by`를 써봅시다.
-    // `remember`는 제거해야 합니다.
-    val (text, setText) = remember { mutableStateOf("") }
-
-    // 단계 5: `toDoList`, `onSubmit`, `onEdit`, `onToggle`,
-    // `onDelete`를 모두 뷰 모델로 옮겨봅시다.
-    val toDoList = remember { mutableStateListOf<ToDoData>() }
+class ToDoViewModel() : ViewModel() {
+    var text by mutableStateOf("")
+    val toDoList = mutableStateListOf<ToDoData>()
 
     val onSubmit: (String) -> Unit = {
         val key = (toDoList.lastOrNull()?.key ?: 0) + 1
         toDoList.add(ToDoData(key, it))
-        setText("")
+        text = ""
     }
 
     val onEdit: (Int, String) -> Unit = { key, newText ->
@@ -92,24 +71,66 @@ fun TopLevel() {
         val i = toDoList.indexOfFirst { it.key == key }
         toDoList.removeAt(i)
     }
+}
+
+// 단계 3: `TopLevel`의 파라미터로 `ToDoViewModel` 타입의
+// `viewModel`을 전달합니다. 기본 값은 `viewModel()`로 설정합시다.
+// 에러가 발생하면 아래의 `import` 문을 추가합니다.
+// `import androidx.lifecycle.viewmodel.compose.viewModel`
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopLevel(viewModel: ToDoViewModel = viewModel()) {
+    // 단계 4: text, setText를 뷰 모델로 옮겨봅시다.
+    // 뷰 모델의 프로퍼티로 변경할 경우에는 destrunction (비구조화,
+    // 구조 분해)는 사용할 수 없으니 `by`를 써봅시다.
+    // `remember`는 제거해야 합니다.
+//    val (text, setText) = remember { mutableStateOf("") }
+
+    // 단계 5: `toDoList`, `onSubmit`, `onEdit`, `onToggle`,
+    // `onDelete`를 모두 뷰 모델로 옮겨봅시다.
+//    val toDoList = remember { mutableStateListOf<ToDoData>() }
+
+//    val onSubmit: (String) -> Unit = {
+//        val key = (toDoList.lastOrNull()?.key ?: 0) + 1
+//        toDoList.add(ToDoData(key, it))
+//        viewModel.text = ""
+//    }
+//
+//    val onEdit: (Int, String) -> Unit = { key, newText ->
+//        val i = toDoList.indexOfFirst { it.key == key }
+//        toDoList[i] = toDoList[i].copy(text = newText)
+//    }
+//
+//    val onToggle: (Int, Boolean) -> Unit = { key, checked ->
+//        val i = toDoList.indexOfFirst { it.key == key }
+//        toDoList[i] = toDoList[i].copy(done = checked)
+//    }
+//
+//    val onDelete: (Int) -> Unit = { key ->
+//        val i = toDoList.indexOfFirst { it.key == key }
+//        toDoList.removeAt(i)
+//    }
 
     Scaffold {
         Column {
             ToDoInput(
-                text = text,
-                onTextChange = setText,
-                onSubmit = onSubmit
+                text = viewModel.text,
+                onTextChange = {
+                    viewModel.text = it
+                },
+                onSubmit = viewModel.onSubmit
             )
             LazyColumn {
                 items(
-                    items = toDoList,
+                    items = viewModel.toDoList,
                     key = { it.key }
                 ) { toDoData ->
                     ToDo(
                         toDoData = toDoData,
-                        onEdit = onEdit,
-                        onToggle = onToggle,
-                        onDelete = onDelete
+                        onEdit = viewModel.onEdit,
+                        onToggle = viewModel.onToggle,
+                        onDelete = viewModel.onDelete
                     )
                 }
             }
